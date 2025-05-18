234fb455239d0b9d68dcc2b30b26ea86a730a2dd
adimerpaul <adimer101@gmail.com>
2025-05-18 08:54:38 -0400

    fucionando metoho put y metho pacth

--- 01 nest/02api/src/users/users.controller.ts
+++ 01 nest/02api/src/users/users.controller.ts
@@ -1,29 +1,36 @@
-import { Controller, Delete, Get, Post, Put, Body, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+  ParseIntPipe,
+  Patch
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService
-  ) {}
+  constructor(private readonly usersService: UsersService) {}
   @Get()
-  find(){
-    return this.usersService.find()
+  find() {
+    return this.usersService.find();
   }
   @Post()
-  store(
-    @Body() body
-  ){
-    return this.usersService.store(body)
+  store(@Body() body) {
+    return this.usersService.store(body);
   }
-  @Put()
-  update(){
-    return 'modificar usuario'
+  @Patch(':id')
+  update(
+    @Param('id',ParseIntPipe) id: number,
+    @Body() body,
+  ) {
+    return this.usersService.update(id, body);
   }
-  @Delete(":id")
-  destroy(
-    @Param('id', ParseIntPipe) id: number
-  ){
-    return this.usersService.destroy(id)
+  @Delete(':id')
+  destroy(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.destroy(id);
   }
 }
--- 01 nest/02api/src/users/users.service.ts
+++ 01 nest/02api/src/users/users.service.ts
@@ -1,18 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  private users = [{id:1, nombre:"Sheldon"}]
-  find(){
-    return this.users
+  private users = [{ id: 1, nombre: 'Sheldon' }];
+  find() {
+    return this.users;
   }
 
-  store(body){
-    return this.users.push({...body, id: this.users.length + 1})
+  store(body) {
+    return this.users.push({ ...body, id: this.users.length + 1 });
   }
-  
-  destroy(id: number){
-    this.users = this.users.filter(user => user.id !== id)
-    return {message: "Usuario eliminado"}
+
+  destroy(id: number) {
+    this.users = this.users.filter((user) => user.id !== id);
+    return { message: 'Usuario eliminado' };
+  }
+  update(id, body){
+    const userFind = this.users.find((user) => user.id === id);
+    if (!userFind) {
+      throw new NotFoundException('Usuario no encontrado');
+    }
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        console.log('user', user);
+        console.log('body', body);
+        console.log('comparacion', { ...user, ...body });
+        return { ...user, ...body };
+      }
+      return user;
+    });
   }
 }
